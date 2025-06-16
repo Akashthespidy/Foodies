959cfb3dd62dffab32b1ccfc31a91110a15aae82
Billal <akashjnu26@gmail.com>
2025-06-16 23:29:07 +0600

    dynamic routing

---
+++ app/meals/[mealSluck]/page.js
@@ -0,0 +1,9 @@
+export default function SlugPage() {
+  return (
+    <div>
+      <h1>Meal Slug Page</h1>
+      <p>Welcome to the meal slug page!</p>
+      <p>This page is dynamically generated based on the meal slug.</p>
+    </div>
+  );
+}
