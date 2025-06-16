4e8cc74b9f17845147e86430623db612588a924d
Billal <akashjnu26@gmail.com>
2025-06-17 00:27:13 +0600

    better orientations

---
+++ components/main-header/main-header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import classes from "./main-header.module.css";
+import logoImg from "../public/images/logo.png";
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+export default function MainHeader() {
+  return (
+    <>
+      <header>
+        <MainHeaderBackground />
+
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="Logo Image " priority />
+          Next level Meals
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+            <li>
+              <Link href="/meals/share">Share Meals</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
