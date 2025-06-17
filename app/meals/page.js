44f64860c53723a282d7685a84c746c1ec556028
Billal <akashjnu26@gmail.com>
2025-06-17 15:56:57 +0600

    outputing meals data and unknown image dimention

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,25 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 export default function MealsPage() {
   return (
-    <div>
-      <h1>Meals Page</h1>
-      <p>Welcome to the meals page!</p>
-    </div>
+    <>
+      <header className="${classes.header}">
+        <h1>
+          Delicious meals created
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p style={{ fontSize: "1.2rem", color: "white", marginTop: "0.5rem" }}>
+          Choose your favorite meals, share your own, and discover new and cook
+          your self.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your meals</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
