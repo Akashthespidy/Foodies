b680175f05dbf5d8217a5bfdebcdf9cf9feeca1e
Billal <akashjnu26@gmail.com>
2025-06-18 12:13:33 +0600

    not found function added

--- app/meals/[mealSluck]/page.js
+++ app/meals/[mealSluck]/page.js
@@ -1,9 +1,33 @@
-export default function SlugPage() {
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+export default function SlugPage({ params }) {
+  const meal = getMeal(params.mealSluck);
+  meal.instructions = meal.instructions.replaceAll("\n", "<br />");
+  if (!meal) {
+    notFound();
+  }
   return (
-    <div>
-      <h1>Meal Slug Page</h1>
-      <p>Welcome to the meal slug page!</p>
-      <p>This page is dynamically generated based on the meal slug.</p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.tittle}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
