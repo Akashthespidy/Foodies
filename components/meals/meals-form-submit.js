e4aad86ea100ef43f3d08f011aac62399040d2f0
Billal <akashjnu26@gmail.com>
2025-06-20 01:03:29 +0600

    using useAction for server actions

--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -1,8 +1,8 @@
 "use client";
 import { use } from "react";
-import { useFormStatus } from "react-dom";
+import { useActionState } from "react";
 export default function MealsFormSubmit() {
-  const { pending } = useFormStatus();
+  const { pending } = useActionState();
   return (
     <button type="submit" disabled={pending}>
       {pending ? "Saving..." : "Share Meal"}
