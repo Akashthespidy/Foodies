ac8c7056b3df66299485dffab31da280c315bff8
Billal <akashjnu26@gmail.com>
2025-06-22 11:03:57 +0600

    fixed all

--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -1,7 +1,7 @@
 "use client";
 import { useFormState, useFormStatus } from "react-dom";
 export default function MealsFormSubmit() {
-  const { pending } = useFormState();
+  const { pending } = useFormStatus();
   return (
     <button type="submit" disabled={pending}>
       {pending ? "Saving..." : "Share Meal"}
